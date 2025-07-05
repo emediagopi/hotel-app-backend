23990c19ed23d2534cd44026c23f7f991d75aab7
gopinathr <gradhakrishnan@adva.com>
2025-07-05 23:45:10 +0530

    initial commit

--- ReadME.md
+++ ReadME.md
@@ -4,3 +4,13 @@ node index.js
 # One Time Activity
 ## Feed the data to DB
 node seed.js
+
+## App Deployment
+https://dashboard.render.com/web/srv-d1km1b95pdvs73b1husg/deploys/dep-d1km1bh5pdvs73b1hvag
+
+
+# Git Commands
+git remote add origin https://github.com/emediagopi/hotel-app-backend.git
+git add .
+git commit -m "initial commit"
+git push -u origin master
--- routes/menu.js
+++ routes/menu.js
@@ -17,4 +17,25 @@ router.post('/', async (req, res) => {
   res.status(201).json(item);
 });
 
+// Update menu item
+router.put('/:id', async (req, res) => {
+  try {
+    const updated = await Menu.findByIdAndUpdate(req.params.id, req.body, { new: true });
+    res.json(updated);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to update menu item' });
+  }
+});
+
+// Delete menu item
+router.delete('/:id', async (req, res) => {
+  try {
+    await Menu.findByIdAndDelete(req.params.id);
+    res.json({ message: 'Item deleted' });
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to delete menu item' });
+  }
+});
+
+
 module.exports = router;
